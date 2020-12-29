931b274554afc623525201d31a215478ebf79082
khanhhongtranle <khanhhong141049@gmail.com>
2020-12-29 08:39:12 +0700

    cake's details

--- ShopCake/Views/CakeDetailView.xaml.cs
+++ ShopCake/Views/CakeDetailView.xaml.cs
@@ -21,9 +21,46 @@ namespace ShopCake.Views
     /// </summary>
     public partial class CakeDetailView : UserControl
     {
+        private DBHelper dBHelper;
+        private Cake cake;
+        private string kind;
+        private int quantity = 1;
+
         public CakeDetailView(Cake item)
         {
             InitializeComponent();
+            this.cake = item;
+            this.dBHelper = new DBHelper();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.DataContext = this.cake;
+            var kindRaw = dBHelper.query($"select * from kindofcakes where id = '{cake.Kind}'", true);
+            this.kind = kindRaw[0]["name"];
+            KindLabel.Content = this.kind;
+        }
+
+        private void Minus_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (this.quantity == 1)
+            {
+                return;
+            }
+            this.quantity--;
+            _quantity.Text = this.quantity.ToString();
+        }
+
+        private void Plus_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.quantity++;
+            _quantity.Text = this.quantity.ToString();
+        }
+
+        private void _editProduct_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            _frame.Children.Clear();
+            _frame.Children.Add(new CakeDetailView(this.cake));
         }
     }
 }
